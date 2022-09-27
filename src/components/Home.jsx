3881f4faaa49447fdb4616119209865513f09066
SuqingLiu <66585292+SuqingLiu@users.noreply.github.com>
2022-09-26 21:11:58 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,9 +9,9 @@ const Home = () => {
       name="home"
       className="h-screen w-full bg-gradient-to-b from-white via-cyan-600 to-white"
     >
-      <div className="max-w-screen-lg mx-auto mt-10 flex flex-col items-center justify-center h-full px-4 md:flex-row">
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white">
+          <h2 className="text-4xl sm:text-7xl mt-10 font-bold text-white">
             I'm a Computer Science Specialist Student at University of Toronto
           </h2>
           <p className="text-yellow-500 py-4 max-w-md">
